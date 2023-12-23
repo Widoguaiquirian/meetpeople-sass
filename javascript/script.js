c34cf75edb5b8506a5c10e432d19d1e76e7fccf4
Luis David Guaiquirian <luisgcode@gmail.com>
2023-12-23 12:58:44 -0800

    Building contact form and deleting some files

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,21 @@
 "use strict";
+// Countries dropDown
+document.addEventListener("DOMContentLoaded", () => {
+  const selectDrop = document.querySelector("#countries");
 
+  fetch("https://restcountries.com/v3.1/all")
+    .then((res) => {
+      return res.json();
+    })
+    .then((countriesList) => {
+      let output = "";
+      countriesList.forEach((country) => {
+        output += `<option>${country.name.common}</option>
+        `;
+      });
+      selectDrop.innerHTML = output;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
